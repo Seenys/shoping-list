73a31ff41d4830a5ac3aaf45ce537dd21e4db2bb
Stephen Barreto <stp.sip7@gmail.com>
2025-03-29 11:59:49 -0500

    functions random

--- client/app/(app)/list/new/index.tsx
+++ client/app/(app)/list/new/index.tsx
@@ -1,12 +1,22 @@
 import { ThemedText } from "@/components/ThemedText";
 import { BodyScrollView } from "@/components/ui/BodyScrollView";
 import IconCircle from "@/components/ui/IconCircle";
+import { backgroundColors, emojies } from "@/constants/Colors";
 import { StyleSheet, View } from "react-native";
 
 const NewListScreen = () => {
+  const randomEmoji = emojies[Math.floor(Math.random() * emojies.length)];
+  const randomColor =
+    backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
+
   return (
     <BodyScrollView contentContainerStyle={{ padding: 16 }}>
-      <IconCircle emoji="🛒" size={60} style={{ alignSelf: "center" }} />
+      <IconCircle
+        emoji={randomEmoji}
+        backgroundColor={randomColor}
+        size={60}
+        style={{ alignSelf: "center" }}
+      />
       <ThemedText type="subtitle">Better Together</ThemedText>
       <ThemedText type="defaultSemiBold" style={styles.subtitle}>
         Create a shared shopping list and collaborate in real-time with your
